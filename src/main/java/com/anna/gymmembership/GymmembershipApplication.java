d839a32ce6cd0987d1c8e375c8f0cf1cf26beaa8
anna <anna.szlapczynska@onet.pl>
2026-04-30 13:53:05 +0200

    Initial commit

---
+++ src/main/java/com/anna/gymmembership/GymmembershipApplication.java
@@ -0,0 +1,13 @@
+package com.anna.gymmembership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymmembershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymmembershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anna/gymmembership/GymmembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anna.gymmembership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymmembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
