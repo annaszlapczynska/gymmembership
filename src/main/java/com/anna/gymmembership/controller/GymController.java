4c5045cd9666a12755c1c556239e74cc58b69405
anna <anna.szlapczynska@onet.pl>
2026-05-02 12:31:06 +0200

    Fourth commit (added creating and listing all gyms)

--- src/main/java/com/anna/gymmembership/controller/GymController.java
+++ src/main/java/com/anna/gymmembership/controller/GymController.java
@@ -1,11 +1,28 @@
 package com.anna.gymmembership.controller;
 
+import com.anna.gymmembership.entity.Gym;
 import com.anna.gymmembership.service.GymService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/gyms")
 public class GymController {
     @Autowired
     private GymService gymService;
+
+    @PostMapping
+    public ResponseEntity<Gym> createGym(@RequestBody Gym gym) {
+        Gym createGym = gymService.createGym(gym);
+        return ResponseEntity.ok(createGym);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Gym>> getAllGyms() {
+        return ResponseEntity.ok(gymService.getAllGyms());
+    }
+
 }
--- src/main/java/com/anna/gymmembership/service/GymService.java
+++ src/main/java/com/anna/gymmembership/service/GymService.java
@@ -1,11 +1,22 @@
 package com.anna.gymmembership.service;
 
+import com.anna.gymmembership.entity.Gym;
 import com.anna.gymmembership.repository.GymRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GymService {
     @Autowired
     private GymRepo gymRepo;
+
+    public Gym createGym(Gym gym) {
+        return gymRepo.save(gym);
+    }
+
+    public List<Gym> getAllGyms() {
+        return gymRepo.findAll();
+    }
 }
